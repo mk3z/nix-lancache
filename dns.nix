3044c0afc3de66469be5c6355c2366bea82984c1
Matias Zwinger <matias@zwinger.fi>
2025-02-11 13:21:50 +0200

    fix: add option to define bind cache networks

--- dns.nix
+++ dns.nix
@@ -1,18 +1,25 @@
-{ cache-domains }: { config, lib, pkgs, ... }:
+{cache-domains}: {
+  config,
+  lib,
+  pkgs,
+  ...
+}:
 with lib;
-with builtins;
-let
+with builtins; let
   cfg = config.lancache.dns;
 
-  domains = filter
+  domains =
+    filter
     (d: d != "" && match "^#.*" d == null)
     (lib.lists.flatten (
       map
-        (f: split "\n" (
-          readFile (cache-domains + "/${f}")
-        )
-        )
-        (filter (f: match ".*txt$" f != null) (attrNames (readDir cache-domains)))
+      (
+        f:
+          split "\n" (
+            readFile (cache-domains + "/${f}")
+          )
+      )
+      (filter (f: match ".*txt$" f != null) (attrNames (readDir cache-domains)))
     ));
 
   ip = cfg.cacheIp;
@@ -30,18 +37,21 @@ ns1     IN  A   ${ip}
 @       IN  A   ${ip}
 *       IN  A   ${ip}
 ";
-in
-{
+in {
   options = {
     lancache.dns = {
       enable = mkEnableOption "Enables the Lancache DNS server";
       forwarders = mkOption {
         description = "Upstream DNS servers. Defaults to CloudFlare and Google public DNS";
         type = with types; listOf str;
-        default = [ "1.1.1.1" "8.8.8.8" ];
+        default = ["1.1.1.1" "8.8.8.8"];
       };
       cacheIp = mkOption {
         description = "IP of cache server to advertise via DNS";
+        type = with types; str;
+      };
+      network = mkOption {
+        description = "Network on which to serve DNS";
         type = with types; str;
       };
     };
@@ -51,12 +61,19 @@ in
     services.bind = {
       enable = true;
       forwarders = cfg.forwarders;
-      cacheNetworks = [ "192.168.0.0/24" "127.0.0.0/24" ];
-      zones = listToAttrs (map (d: { name = d; value = { master = true; file = zonefile; }; }) (domains));
+      cacheNetworks = [cfg.network "127.0.0.0/24"];
+      zones = listToAttrs (map (d: {
+          name = d;
+          value = {
+            master = true;
+            file = zonefile;
+          };
+        })
+        domains);
     };
 
-    networking.firewall.allowedTCPPorts = [ 53 ];
-    networking.firewall.allowedUDPPorts = [ 53 ];
+    networking.firewall.allowedTCPPorts = [53];
+    networking.firewall.allowedUDPPorts = [53];
     networking.resolvconf.useLocalResolver = true;
   };
 }
